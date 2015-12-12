db94774700bea2fc9a4d32b3565eb15e86e4dd2f
sndnvaps <sndnvaps@gmail.com>
2015-12-12 23:16:20 +0800

    sync from CyanogenMod and add twrp support
    Change-Id: I6cde676b2977874487e135878153c78f3f56fcff
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,9 @@ BOARD_SEPOLICY_UNION += \
     wpa.te \
     file_contexts \
     genfs_contexts
-
+# twrp support 
+BOARD_SEPOLICY_UNION += \
+	recovery.te
 HAVE_ADRENO_SOURCE:= false
 
 OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
@@ -168,6 +170,20 @@ TARGET_HW_DISK_ENCRYPTION := true
 RECOVERY_FSTAB_VERSION := 2
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+
+#TWRP
+DEVICE_RESOLUTION := 1080x1920
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_USB_STORAGE := false
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+# The real path for this is /sys/devices/mdp.0/qcom,cmdss_fb_primary.160/leds/lcd-backlight/brightness but the comma doesn't compile correctly
+TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.160/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_NO_SCREEN_TIMEOUT := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+
 
 # Hardware
 BOARD_HARDWARE_CLASS := device/lge/hammerheadcaf/cmhw
